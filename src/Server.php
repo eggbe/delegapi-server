e7457232213151bb11dc33ae900b2c688833065e
hacpaka <hacpaka@eggbe.com>
2016-07-26 10:26:31 +0300

    Simple callbacks are available now!

--- src/Server.php
+++ src/Server.php
@@ -25,17 +25,19 @@ class Server {
 
 	/**
 	 * @param string $name
-	 * @param AListener $Listener
+	 * @param \Closure|AListener $Listener
 	 * @return Server
 	 * @throws \Exception
 	 */
-	public function listen($name, AListener $Listener){
-		if (!in_array(($name = strtolower(trim($name))), [self::ON_EXECUTE,
-			self::ON_AUTHORIZE])) {
-				throw new \Exception('Unknown listening action "' . $name . '"!');
+	public function listen($name, $Listener) {
+		if (!is_subclass_of($Listener, AListener::class) && !is_a($Listener, \Closure::class)){
+			throw new \Exception('Invalid listener for listing action "' . $name . '"!');
+		}
+		if (!in_array(($name = strtolower(trim($name))), [self::ON_EXECUTE, self::ON_AUTHORIZE])) {
+			throw new \Exception('Unknown listening action "' . $name . '"!');
 		}
 
-		if (array_key_exists($name, $this->Listeners)){
+		if (array_key_exists($name, $this->Listeners)) {
 			throw new \Exception('Can not reassign listener for listening action "' . $name . '"!');
 		}
 
@@ -47,11 +49,11 @@ class Server {
 	 * @param array $Input
 	 * @return string
 	 */
-	public function dispatch(array $Input){
+	public function dispatch(array $Input) {
 		$Bridge = new Bridge();
 
 		/**
-		 *	We have to provide the secret key to be authorized.
+		 *    We have to provide the secret key to be authorized.
 		 *  If authorization key is not found an exception will thrown immediately.
 		 */
 		$Bridge->on('!key', function () {
@@ -64,8 +66,8 @@ class Server {
 		 *
 		 * In case when the listener is not assigned all keys will be accepted.
 		 */
-		$Bridge->on('key', function($hash){
-			if (key_exists(self::ON_AUTHORIZE, $this->Listeners) && !$this->Listeners[self::ON_AUTHORIZE]($key)){
+		$Bridge->on('key', function ($hash) {
+			if (key_exists(self::ON_AUTHORIZE, $this->Listeners) && !$this->Listeners[self::ON_AUTHORIZE]($key)) {
 				throw new \Exception('Access denied!');
 			}
 		});
@@ -76,7 +78,7 @@ class Server {
 		 *
 		 * The action name and namespace are required.
 		 */
-		$Bridge->on(['namespace', 'method', ':params'], function($namespace, $method, $Params = []){
+		$Bridge->on(['namespace', 'method', ':params'], function ($namespace, $method, $Params = []) {
 			return array_key_exists(self::ON_EXECUTE, $this->Listeners)
 				? $this->Listeners[self::ON_EXECUTE]($namespace, $method, $Params) : false;
 		});
