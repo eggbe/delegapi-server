ae122b332a22e8dd144b5cb79b5e714b38127c24
hacpaka <hacpaka@eggbe.com>
2016-07-28 14:19:09 +0300

    +

--- src/Server.php
+++ src/Server.php
@@ -53,21 +53,21 @@ class Server {
 		$Bridge = new Bridge();
 
 		/**
-		 *    We have to provide the secret key to be authorized.
-		 *  If authorization key is not found an exception will thrown immediately.
+		 * We have to provide the security token to be authorized.
+		 * If authorization token is not found an exception will thrown immediately.
 		 */
 		$Bridge->on('!token', function () {
-			throw new \Exception('Access key is not found!');
+			throw new \Exception('Access token is not found!');
 		});
 
 		/**
-		 * If an authorisation key is provided it have to been checked via special listener method.
+		 * If an authorisation token is provided it have to been checked via special listener method.
 		 * This method should return a not false value to continue. Otherwise an exception will be thrown immediately.
 		 *
 		 * In case when the listener is not assigned all keys will be accepted.
 		 */
-		$Bridge->on('token', function ($key) {
-			if (key_exists(self::ON_TOKEN, $this->Listeners) && !$this->Listeners[self::ON_TOKEN]($key)) {
+		$Bridge->on('token', function ($token) {
+			if (key_exists(self::ON_TOKEN, $this->Listeners) && !$this->Listeners[self::ON_TOKEN]($token)) {
 				throw new \Exception('Access denied!');
 			}
 		});
